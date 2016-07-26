bc6edebe3980b3d544c91a75fcea8ef9c1e27ff4
Diego Kiner <diego-k@berkeley.edu>
2016-07-26 12:44:09 -0700

    Optional sigma in solve_bidual.m

--- solve_bidual.m
+++ solve_bidual.m
@@ -1,9 +1,9 @@
 %% Load data from Amazon reviews
-load 'kernels.mat'
+load 'data/kernels.mat'
 kernels = {unigram_kernel, bigram_kernel, trigram_kernel};
 n = size(unigram_kernel, 1);
 p = 10;
-rho = 0.05;
+rho = 0.05;c
 
 [K, D] = eigs(kernels{1}, p);
 for i = 1:size(K, 2)
@@ -17,37 +17,41 @@ for i = 2:size(kernels, 2)
     K = [K, V];
 end
 m = size(K, 2);
+y = eye(n);
 
 %% Load random kernel vectors
-load('random_kernel.mat')
+load('data/random_kernel.mat')
 rho = 0.05;
-
-%% Sovle bidual
 n = size(K, 1);
 m = size(K, 2);
+y = eye(n);
 
+%% Augment matrices if desired
+sigma = rho/20;
+K = [K; sigma*eye(m)];
+y = [eye(n); zeros(m,n)];
+
+%% Sovle bidual
 cvx_begin
     variable U(m,n);
-    variable V(n,n);
     
     t1 = 0;
     for i = 1:m
         t1 = t1 + norm(U(i,:));
     end
     
-    t2 = eye(n);
+    t2 = y;
     for i = 1:m
         t2 = t2 - K(:,i) * U(i,:);
     end
     
-    minimize ( rho * t1 + norm(V) )
-    V == t2;
+    minimize ( rho * t1 + norm(t2) )
     
 cvx_end
 
 %% Recover primal weights
 lambda = zeros(m+1,1);
-lambda(1) = (1/cvx_optval) * norm(V);
+lambda(1) = (1/cvx_optval) * norm(t2);
 for i = 1:m
     lambda(i+1) = (rho/cvx_optval) * norm(U(i,:));
 end
