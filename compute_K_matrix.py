Fixed trace computation

compute_K_matrix.py
@@ -1,16 +1,20 @@
 import sys
+import random
 from kernel_util import *
 
-raw_corpus = sys.argv[1]
-if raw_corpus[-5:] == '.json':
-    corpus = parse_json(raw_corpus)
-else:
-    print "File type not supported."
-    exit(0)
+corpus = []
+for n in range(1, len(sys.argv)-1):
+    raw_corpus = sys.argv[n]
+    if raw_corpus[-5:] == '.json':
+        part_corpus = parse_json(raw_corpus)
+        corpus += part_corpus
+    else:
+        print "File type not supported."
+        exit(0)
 
-corpus = corpus[:100]
+corpus = random.sample(corpus, 3000)
 p = 10
-kernels = compute_ngram_kernels(corpus, 3)
+kernels = compute_ngram_kernels(corpus, 1)
 K = dyad_library(kernels, p)
-np.save(sys.argv[2], K)
+np.save(sys.argv[-1], K)
 

umkl_descent.py
@@ -16,7 +16,7 @@ def umkl_descent(K, rho, epsilon=0.001, sigma=None):
     m = K.shape[1]
     k_norms = [norm(K[:,i]) for i in range(m)]
     K /= sum(k_norms)
-    print 'Peforming UMKL for ' + str(n) + ' X ' + str(n) + ' kernels.'
+    print 'Peforming UMKL for ' + str(m) + ' kernels of dimension ' + str(n)
 
     # Eliminate k_i with norm < rho
     to_delete = []
@@ -110,9 +110,9 @@ def umkl_descent(K, rho, epsilon=0.001, sigma=None):
         K = K[:n,:]
     optimal_kernel = weights[0]*np.eye(n)
     for i in range(m):
-        optimal_kernel += (rho**(-2)) * weights[i] * np.outer(K[:,i], K[:,i])
-
-    trace = np.trace(np.linalg.inv(optimal_kernel))
+        optimal_kernel += (rho**(-2)) * weights[i+1] * np.outer(K[:,i], K[:,i])
+    
+    trace = sum( [(1.0/eig) for eig in np.linalg.eig(optimal_kernel)[0]] )
     
     return weights, trace, objective_values
     